603da03c1641746ed62393770c7fc541ea0ad2b0
Rafaello <baimurzinrafael02@gmail.com>
2022-03-11 22:45:02 +0600

    Created Main view

--- app/src/main/java/com/example/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/inventoryapp/MainActivity.java
@@ -2,16 +2,127 @@ package com.example.inventoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.inventoryapp.data.InventoryContract;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int ITEM_LOADER = 0;
+
+    InventoryCursorAdapter cursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FloatingActionButton fab =findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+            startActivity(intent);
+        });
 
+        ListView inventoryListView = findViewById(R.id.list);
 
+        View emptyView = findViewById(R.id.empty_view);
+        inventoryListView.setEmptyView(emptyView);
+
+        cursorAdapter = new InventoryCursorAdapter(this, null);
+        inventoryListView.setAdapter(cursorAdapter);
+
+        inventoryListView.setOnItemClickListener(((adapterView, view, position, id) -> {
+            Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+            Uri currentInventoryUri = ContentUris.withAppendedId(InventoryContract.InventoryEntry.CONTENT_URI, id);
+            intent.setData(currentInventoryUri);
+            startActivity(intent);
+        }));
+
+        getLoaderManager().initLoader(ITEM_LOADER, null, this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private void deleteAllItems() {
+        int rowsDeleted = getContentResolver().delete(InventoryContract.InventoryEntry.CONTENT_URI, null, null);
+        Log.v("CatalogActivity", rowsDeleted + " rows deleted from pet database");
+    }
+
+    private void insertItem() {
+        ContentValues values = new ContentValues();
+        values.put(InventoryContract.InventoryEntry.COLUMN_ITEM_NAME, "Camera");
+        values.put(InventoryContract.InventoryEntry.COLUMN_ITEM_PRICE, 9.99);
+        values.put(InventoryContract.InventoryEntry.COLUMN_ITEM_QUANTITY, 10);
+        values.put(InventoryContract.InventoryEntry.COLUMN_ITEM_SUPPLIER, "Rafael Baimurzin");
+        values.put(InventoryContract.InventoryEntry.COLUMN_ITEM_EMAIL, "baimurzinrafael02@gmail.com");
+
+        Uri newUri = getContentResolver().insert(InventoryContract.InventoryEntry.CONTENT_URI, values);
+
+        if(newUri == null) {
+            Toast.makeText(this, getString(R.string.editor_insert_item_failed),
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, getString(R.string.editor_insert_item_successful),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_inventory, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_insert_dummy_data:
+                insertItem();
+                return true;
+            case R.id.action_delete_all_entries:
+                deleteAllItems();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        String[] projection = {
+                InventoryContract.InventoryEntry._ID,
+                InventoryContract.InventoryEntry.COLUMN_ITEM_NAME,
+                InventoryContract.InventoryEntry.COLUMN_ITEM_PRICE,
+                InventoryContract.InventoryEntry.COLUMN_ITEM_QUANTITY
+        };
+
+        return new CursorLoader(this, InventoryContract.InventoryEntry.CONTENT_URI, projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        cursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null);
     }
 }
